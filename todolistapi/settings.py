1546e27a63e2bbfc5310ba785a056ae1a155f209
Maveronic <mmesomamgbemena@yahoo.com>
2022-11-09 04:36:19 +0300

    Changed placeholder in allowed hosts

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('TODO_LIST_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'authentication.User'
 
